38f76736151ee9584ff96914d298cff62899ddf2
zaiboot <zaiboot@gmail.com>
2017-01-10 13:54:50 -0600

    Updating gitignore and adding gulp

--- Training/demo01/app.js
+++ Training/demo01/app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
 
-var port = 50000;
+var port = process.env.PORT || 50000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
     res.send('hello world');
-}).get('/books', function(req, res){
+}).get('/books', function(req, res) {
 
     res.send('Hello Books');
-} );
-app.listen(port, function(err){
+});
+app.listen(port, function(err) {
     console.log('Running server on port', port);
 });
